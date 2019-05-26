e6fd45d16b9c441416f61219d009b3be29ab2101
Baudouin de Truchis <baudouin@detruchis.com>
2019-05-27 01:38:02 +0800

    update

--- web_scraping.py
+++ web_scraping.py
@@ -16,91 +16,79 @@ headers = {
     }
 
 urls = ['https://www.bbc.com/news/business-38507481'
-#        , 'https://www.bbc.com/news/business/companies'
-#        , 'https://www.bbc.com/news/business/economy'
-#        , 'https://www.bbc.com/news/business'
+        , 'https://www.bbc.com/news/business/companies'
+        , 'https://www.bbc.com/news/business/economy'
+        , 'https://www.bbc.com/news/business'
         ]
 
 logging.info('START')
 
-# test connection
-response = requests.get(url, headers=headers, timeout=5)
-if response.status_code != 200:
-    logging.warning(response)
-
-page_soup = BeautifulSoup(response.text, 'lxml')
-
-nav_bar = page_soup.find('ul', {'class':'gs-o-list-ui--top-no-border nw-c-nav__wide-sections'}).findAll('a')
-
-cats = []
-pages = []
+df = pd.DataFrame(columns=['date','title','summary','link'])
 
 for url in urls:
-    cats.append(cat.get('href'))
-    
-for cat in cats:
-    url = main_url + cat
+
+    # test connection
     response = requests.get(url, headers=headers, timeout=5)
     if response.status_code != 200:
         logging.warning(response)
-        
+
     page_soup = BeautifulSoup(response.text, 'lxml')
-    try:
-        sub_nav = page_soup.find('ul', {'class':'gs-o-list-ui--top-no-border nw-c-nav__secondary-sections'}).findAll('a')
-    except:
-        try:
-            sub_nav = page_soup.find('ul', {'class':'js-navigation-panel-secondary'}).findAll('a')
-        except:
-            pass
-    for page in sub_nav:
-        pages.append(main_url + page.get('href'))
-    print(1)
-    
 
-
-#grab every article in 'latest updates'
-containers = page_soup.find_all('article', {'class':'lx-stream-post gs-u-align-left lx-stream-post--has-meta'})
-
-articles = pd.DataFrame(columns=['date','title','summary'])
-
-for container in containers:
-    # get creation date - if no => take current one at UTC
-    try:
-        article_date = container.div.time.find('span', {'class':'qa-meta-date gs-u-mr gs-u-display-inline-block'}).text
-    except:
-        article_date = datetime.datetime.utcnow().strftime('%d %B')
-
-    # get creation time
-    try:
-        article_time = container.div.findAll('span')[4].text
-    except:
-        article_time = ''
-
-    # parse into datetime
-    article_datetime = datetime.datetime.strptime(
-            datetime.datetime.utcnow().strftime('%Y')
-            + article_date + article_time, '%Y%d %B%H:%M')
-
-    # grab article title
-    try:
-        article_title = container.find('span', {'class':'lx-stream-post__header-text'}).text
-    except:
-        article_title = ''
-
-    # grab article summary
-    try:
-        article_summary = container.findAll('p')[-1].text
-    except:
-        try:
-            article_summary = container.find('pre').text
-        except:
-            article_summary = ''
-
-    articles = articles.append({'date':article_datetime,
-                                'title':article_title,
-                                'summary':article_summary}, ignore_index=True)
-
-print(articles)
+    df = pd.concat((df,get_articles(page_soup)), axis=0)
 
 logging.info('FINISHED')
 
+
+def get_articles(page_soup):
+    """
+    grab every article in 'latest updates'
+    """
+    containers = page_soup.find_all('article', {'class':'lx-stream-post gs-u-align-left lx-stream-post--has-meta'})
+
+    articles = pd.DataFrame(columns=['date','title','summary','link'])
+
+    for container in containers:
+        # get creation date - if no => take current one at UTC
+        try:
+            article_date = container.div.time.find('span', {'class':'qa-meta-date gs-u-mr gs-u-display-inline-block'}).text
+        except:
+            article_date = datetime.datetime.utcnow().strftime('%d %B')
+
+        # get creation time
+        try:
+            article_time = container.div.findAll('span')[4].text
+        except:
+            article_time = ''
+
+        # parse into datetime
+        article_datetime = datetime.datetime.strptime(
+                datetime.datetime.utcnow().strftime('%Y')
+                + article_date + article_time, '%Y%d %B%H:%M')
+
+        # grab article title
+        try:
+            article_title = container.find('span', {'class':'lx-stream-post__header-text'}).text
+        except:
+            article_title = ''
+
+        # grab article summary
+        try:
+            article_summary = container.findAll('p')[-1].text
+        except:
+            try:
+                article_summary = container.find('pre').text
+            except:
+                article_summary = ''
+
+        # grab article link
+        try:
+            article_link = container.find('a').get('href')
+        except:
+            article_link = ''
+
+        #save article info in a dataframe
+        articles = articles.append({'date':article_datetime,
+                                'title':article_title,
+                                'summary':article_summary,
+                                'link':article_link}, ignore_index=True)
+    return articles
